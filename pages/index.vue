ee26f521d5404249a9ca237a7401a251d4459cb6
Carlos <geop0p3@gmail.com>
2022-01-10 18:27:22 -0800

    Set up main project

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button
+      type="primary"
+      size="large"
+      :icon="Search"
+      @click="$refs.form.changeVisibility()"
+      >Agregar Pago</el-button
+    >
+    <AddPayment ref="form" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      dialogVisibility: false,
+    }
+  },
+}
+</script>
